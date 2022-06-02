16e2236be5ba4f878eaf6e66a6ef30cbcce05991
TomxHWRxWI20C <84907054+TomxHWRxWI20C@users.noreply.github.com>
2022-06-02 12:45:29 +0200

    sider styling

--- src/layout/sider/AppSider.js
+++ src/layout/sider/AppSider.js
@@ -1,10 +1,33 @@
 import React from "react"
-import style from "../Layout.module.css"
+import layout from "../Layout.module.css"
+import style from "./AppSider.module.css"
+import moment from 'moment'
+
 
 export default function AppSider() {
   return (
-    <div className={style.appsider}>
-      <h1>Nicos Infos</h1>
+    <div className={layout.appsider}>
+      <h1>{moment().format('DD.MM.YYYY')}</h1>
+      <div className={style.container}>
+        <h2>Soll-Arbeitszeit</h2>
+        <h2 className={style.time}>08:00</h2>
+      </div>
+      <div className={style.container}>
+        <h2>Mindestpausenzeit</h2>
+        <h2 className={style.time}>00:30</h2>
+      </div>      
+      <div className={style.container}>
+        <h2> Geplantes Arbeitsende </h2>
+        <h2 className={style.disabled}>00:00</h2>
+      </div>      
+      <div className={style.container}>
+        <h2>Ist-Arbeitszeit</h2>
+        <h2 className={style.disabled}>00:00</h2>
+      </div>      
+      <div className={style.container}>
+        <h2>Arbeitszeitkonto</h2>
+        <h2 className={style.disabled}>00:00</h2>
+      </div>
     </div>
   )
 }
