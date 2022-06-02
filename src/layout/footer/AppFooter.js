74d0acb60e312530cc8409425db826fef7fdb0bf
TomxHWRxWI20C <84907054+TomxHWRxWI20C@users.noreply.github.com>
2022-06-02 14:51:07 +0200

    Footer links + styling

--- src/layout/footer/AppFooter.js
+++ src/layout/footer/AppFooter.js
@@ -1,12 +1,23 @@
 import React from "react"
-import classes from "../Layout.module.css"
+import layout from "../Layout.module.css"
+import style from "./AppFooter.module.css"
+import logo from "../../images/GithubLogo.png"
 
 export default function AppFooter() {
   return (
-    <div className={classes.appfooter}>
-      <h5>IMPERIO!</h5>
-      <h3>CRUCIO!</h3>
-      <h1>AVADA KEDAVRA!</h1>  
+    <div className={layout.appfooter}> 
+      <a className={style.link} href="https://github.com/Stier-09/hwr-zeiterfassung-desktopfrontend">
+        <img className={style.logo} src={logo} alt="Github Logo" />
+        Desktopclient Repository
+      </a>
+      <a className={style.link} href="https://github.com/Stier-09/hwr-zeiterfassung-webfrontend">
+        <img className={style.logo} src={logo} alt="Github Logo" />
+        Website Repository
+      </a>
+      <a className={style.link} href="https://github.com/Stier-09/hwr-zeiterfassung-backend">
+        <img className={style.logo} src={logo} alt="Github Logo" />
+        Backend Repository
+      </a>
     </div>
   )
 }
